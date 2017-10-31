815242158b8919a8feacd21e3d1c9626a1c81a78
Маркиян <mark0611@ukr.net>
2017-10-31 11:11:53 +0200

    Add project files.

---
+++ CurrencyLayerMobile/CurrencyLayerMobile/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CurrencyLayerMobile
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
